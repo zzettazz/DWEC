b1fb72ef8b825dbbac684e2a3bfb87ee7cdcce61
zzettazz <junquerdaviddaw@gmail.com>
2023-10-19 14:14:17 +0200

    fixes

--- Tema2/EjercicioAplicacionVariados/script.js
+++ Tema2/EjercicioAplicacionVariados/script.js
@@ -1,6 +1,10 @@
+var serv = window.location.host;
+var puertoFix = (serv.substring((serv.length-4),serv.length));
+
 var aMostrar = "";
 aMostrar = aMostrar.concat("URL Completa: || "+window.location.href+"<br/>");
 aMostrar = aMostrar.concat("Pathname: || "+window.location.pathname+"<br/>");
-aMostrar = aMostrar.concat("Protocolo/Puerto: || "+window.location.protocol+"<br/>");
-aMostrar = aMostrar.concat("Servidor/Hostname: || "+window.location.host+"<br/>");
+aMostrar = aMostrar.concat("Protocolo: || "+window.location.protocol+"<br/>");
+aMostrar = aMostrar.concat("Puerto: || "+puertoFix+"<br/>")
+aMostrar = aMostrar.concat("Servidor/Hostname: || "+(serv.substring(0,(serv.length-5)))+"<br/>");
 document.getElementById("resultado").innerHTML=aMostrar;
