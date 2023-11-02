c20b9fc1b93fbe1005f2597d039dfb8376364cd6
zzettazz <junquerdaviddaw@gmail.com>
2023-11-02 14:16:17 +0100

    a

--- TEMA 3 - Programación con arrays, funciones y objetos definidos por el usuario/Práctica/Ejercicio Imágenes/script.js
+++ TEMA 3 - Programación con arrays, funciones y objetos definidos por el usuario/Práctica/Ejercicio Imágenes/script.js
@@ -1,4 +1,4 @@
-var miArray = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.png","13.jpg","14.png","15.jpg"]
+var miArray = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.png","13.jpg","14.png","15.jpg"];
 
 function siguiente()
 {
