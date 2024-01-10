ba56a09a291232eebf8f5c358330d92084e87764
zzettazz <junquerdaviddaw@gmail.com>
2024-01-10 12:36:37 +0100

    a

--- Material ayuda/Tema 1 - (JS básico)/Ejercicios Básicos.js
+++ Material ayuda/Tema 1 - (JS básico)/Ejercicios Básicos.js
@@ -3,5 +3,8 @@
 // Ejercicio 1
 // Hacer un "Hola mundo" por consola
 
+
+
 // Ejercicio 2
 // Hacer un "Hola mundo" en un archivo HTML, pero usando JS
+
---
+++ Material ayuda/Tema 2 - (Uso de objetos-funciones predefinidas de JS)/Ejercicios Objetos.js
@@ -0,0 +1,3 @@
+// Ejercicio 1
+// Hacer una caja de texto y un botón, y cuando se presione el botón aparezca en la caja de texto "Hola"
+// Pista: Debes crear el código HTML de ambos y luego introducirlo en el html
