dbb19c3a1792c7c1d05b323f315f34b265ec807e
zzettazz <junquerdaviddaw@gmail.com>
2024-01-10 12:34:27 +0100

    ej

--- Material ayuda/Tema 1 - (JS básico)/Ejercicios Básicos.js
+++ Material ayuda/Tema 1 - (JS básico)/Ejercicios Básicos.js
@@ -5,7 +5,3 @@
 
 // Ejercicio 2
 // Hacer un "Hola mundo" en un archivo HTML, pero usando JS
-
-// Ejercicio 3
-// Hacer una caja de texto y un botón, y cuando se presione el botón aparezca en la caja de texto "Hola"
-
