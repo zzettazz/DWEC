fd0fb0f14240583d3f3c7a66cab258aae6e6259f
zzettazz <junquerdaviddaw@gmail.com>
2024-01-10 12:33:24 +0100

    .

---
+++ Material ayuda/Tema 1 - (JS básico)/Ejercicios Básicos.js
@@ -0,0 +1,11 @@
+// Ejercicios básicos para aprender a usar JS
+
+// Ejercicio 1
+// Hacer un "Hola mundo" por consola
+
+// Ejercicio 2
+// Hacer un "Hola mundo" en un archivo HTML, pero usando JS
+
+// Ejercicio 3
+// Hacer una caja de texto y un botón, y cuando se presione el botón aparezca en la caja de texto "Hola"
+
